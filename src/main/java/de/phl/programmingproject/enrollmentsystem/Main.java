74fa69d88f95bd6561649c0a9eeb0346510fac82
TrisiJo <w88t4prnn6@privaterelay.appleid.com>
2025-11-09 12:37:11 +0100

    Aufgabe: Student-enrolment-system

--- src/main/java/de/phl/programmingproject/enrollmentsystem/Main.java
+++ src/main/java/de/phl/programmingproject/enrollmentsystem/Main.java
@@ -1,10 +1,74 @@
 package de.phl.programmingproject.enrollmentsystem;
 
+import java.util.stream.Collectors;
+
 /**
  * PUT YOUR CODE FOR THE EXERCISE 'STUDENT ENROLLMENT SYSTEM' IN THIS CLASS
  */
 public class Main {
     public static void main(String[] args) {
-        // TODO: implement your program here.
-    }
+        /* 1. Student Objekt  */
+        final Student john = new Student("John Doe", "12345"); 
+       // 2. Course-Objekt
+        final Course intro = new Course("Introduction to Computer Science");
+
+        /* 3. Infos ausgeben */ 
+        System.out.println("=== Vor der Einschreibung ===");
+        System.out.println("Student: " + john.getInfo());
+        System.out.println("Kurs:    " + intro.getInfo());
+
+        // 4. Einschreiben & Enrollment merken
+        final Enrollment johnInIntro = john.enroll(intro);
+
+        // 5. Infos ausgeben
+        System.out.println("\n=== Nach der Einschreibung ===");
+        System.out.println("Student: " + john.getInfo());
+        System.out.println("Kurs:    " + intro.getInfo());
+
+        // 6. Enrollment-Infos
+        System.out.println("\n=== Enrollment-Info ===");
+        System.out.println(johnInIntro.getInfo());
+
+        /* 7. Note setzen */ 
+        johnInIntro.setGrade(4.0);
+
+        // 8. Enrollment-Infos erneut
+        System.out.println("\n=== Enrollment-Info (mit Note) ===");
+        System.out.println(johnInIntro.getInfo());
+
+        // 9.  Zweiten Kurs anlegen
+        final Course oop = new Course("Object-Oriented Programming");
+        System.out.println("\n=== Zweiter Kurs erstellt ===");
+        System.out.println("Kurs:    " + oop.getInfo());
+        // 10. Studenten in den neuen Kurs einschreiben
+        final Enrollment johnInOop = john.enroll(oop);
+        System.out.println("\n=== 10) Einschreibung in Kurs 2 ===");
+        System.out.println(johnInOop.getInfo());
+
+        // 11. Ausgabe der Studenten  (getCourses)
+        System.out.println("\n=== 11) Kurse des Studenten (getCourses) ===");
+        String coursesOfJohn = john.getCourses().stream()
+                .map(Course::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println("Kurse von " + john.getName() + ": [" + coursesOfJohn + "]");
+
+        // 12.  Liste der Studierenden im ersten Kurs (getStudents)
+        System.out.println("\n=== 12) Studierende im ersten Kurs (getStudents) ===");
+        String studentsInIntro = intro.getStudents().stream()
+                .map(Student::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println("Studierende in '" + intro.getName() + "': [" + studentsInIntro + "]");
+
+        // 13.  Studenten aus dem ersten Kurs streichen (drop)
+        System.out.println("\n=== 13) Drop aus erstem Kurs ===");
+        intro.drop(john);
+
+        // 14. Liste der Studierenden im ersten Kurs erneut ausgeben
+        System.out.println("\n=== 14) Studierende im ersten Kurs nach Drop ===");
+        String studentsInIntroAfter = intro.getStudents().stream()
+                .map(Student::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println("Studierende in '" + intro.getName() + "': [" + studentsInIntroAfter + "]");
+
+    } 
 }
