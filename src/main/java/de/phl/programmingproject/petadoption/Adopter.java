90623ed04fa86ea07746eed1c31bf6ac27ebd41f
ojj407 <marie.pfuderer@stud.ph-ludwigsburg.de>
2025-11-10 11:13:52 +0100

    Adopter

--- src/main/java/de/phl/programmingproject/petadoption/Adopter.java
+++ src/main/java/de/phl/programmingproject/petadoption/Adopter.java
@@ -11,9 +11,9 @@ public class Adopter {
 
     /**
      * Creates a new adopter with the given name, phone and email.
-     * @param name The name of the adopter.
-     * @param phone The phone number of the adopter.
-     * @param email The email of the adopter.
+     * @param Jane_Smith The name of the adopter.
+     * @param 555-555-5555 The phone number of the adopter.
+     * @param janesmith@email.com The email of the adopter.
      * @throws IllegalArgumentException If the name is null or empty, the phone is null or empty or the email is null or empty.
      */
     public Adopter(final String name, final String phone, final String email) {
